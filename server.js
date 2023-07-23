b28ca11f5868264dde2f0cf86ccbc4dddd21a7cc
Swarnendu05 <swarnendumondal2012@gmail.com>
2023-07-23 11:54:18 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passportConfig = require("./lib/passportConfig");
+const cors = require("cors");
+const fs = require("fs");
+
+
+mongoose
+  .connect("mongodb://localhost:27017/globsynJobportal", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((res) => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
+}
+if (!fs.existsSync("./public/profile")) {
+  fs.mkdirSync("./public/profile");
+}
+
+const app = express();
+const port = 4444;
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use(cors());
+app.use(express.json());
+app.use(passportConfig.initialize());
+
+  app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
+app.use("/upload", require("./routes/uploadRoutes"));
+app.use("/host", require("./routes/downloadRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
